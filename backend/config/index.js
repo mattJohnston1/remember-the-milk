3298cfe911d451c85b8f5a47a890f245cb8f908d
emollientspy1 <emollientspy1@gmail.com>
2021-05-04 11:01:22 -0600

    working on css

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,9 +3,9 @@ module.exports = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
   db: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    username: "auth_app",
+    password: "password",
+    database: "auth_db",
     host: process.env.DB_HOST,
   },
   jwtConfig: {
